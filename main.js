2c7e13a78c9f2a1cb81aa277c6061c969ea8c40b
Mark Protsyuk <protsyukmark@gmail.com>
2025-08-25 21:04:01 -0700

    defined the road and added lines and a cool background

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ canvas.width = 200;
 
 // Canvas Context for the car, this will contain all of the method that we need to draw what we need to draw
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -14,6 +15,10 @@ function animate() {
   // By moving this line of code to this function, we are able to over come the issue of the road not going from top to bottow, and the car leaving a trail
   // This is because both are updated on the animate funciton, therefore being refreshed
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.8);
+  road.draw(ctx);
   car.draw(ctx);
   // Calls the animation again and again, giving the illusion of movement
   requestAnimationFrame(animate);
