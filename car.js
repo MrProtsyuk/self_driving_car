1867e912492651bde52d6dbed56edce154a490dc
Mark Protsyuk <protsyukmark@gmail.com>
2025-08-25 18:02:42 -0700

    implemented full driving mechanics

--- car.js
+++ car.js
@@ -8,36 +8,86 @@ class Car {
     this.width = width;
     this.height = height;
 
+    // We need to create speed and acceleration
+    this.speed = 0;
+    this.acceleration = 0.2;
+    // As well as friction
+    // This max speed creates an overflow when we start to go left or right, making us actually go above the speed
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
+    // Using angles to alter turning speed
+    this.angle = 0;
+
     // Controls from the controls.js file
     this.controls = new Controls();
   }
 
   update() {
+    this.#move();
+  }
+
+  #move() {
     if (this.controls.forward) {
       // Remember that the controls on a computer are inverse
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
-    if (this.controls.right) {
-      this.x -= 2;
+    // Capping the speed at the max
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
     }
-    if (this.controls.left) {
-      this.x += 2;
+    // Negative sign only represents that the car is going backwards
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
     }
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    // Because of the if statement above, the car is constantly slippling between negative and postive value, therefore it is slowly moving up
+    // This if statement, checks if the value is less than the friction, if so, the car just stops moving its position
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      // This 0.03 value works because of the unit circle, but this unit circle is rotated 90 degrees
+      // So it is a little funky
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+    // Using trigonometry, we can make the car turn while driving, not just rotate in place
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   // Method that gets the context passed through
   draw(ctx) {
+    // The following calls are made to rotate the car so that it can turn
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    // Begins the drawing of the car context
     ctx.beginPath();
     ctx.rect(
       // The x of the car will be the center of the car inside
-      this.x - this.width / 2,
-      this.y - this.height / 2,
+      -this.width / 2,
+      -this.height / 2,
       this.width,
       this.height
     );
     ctx.fill();
+
+    // This will stop from the car from doing infinite rotation
+    ctx.restore();
   }
 }
--- main.js
+++ main.js
@@ -6,4 +6,15 @@ canvas.width = 200;
 // Canvas Context for the car, this will contain all of the method that we need to draw what we need to draw
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
+
+animate();
+
+function animate() {
+  car.update();
+  // By moving this line of code to this function, we are able to over come the issue of the road not going from top to bottow, and the car leaving a trail
+  // This is because both are updated on the animate funciton, therefore being refreshed
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+  // Calls the animation again and again, giving the illusion of movement
+  requestAnimationFrame(animate);
+}
